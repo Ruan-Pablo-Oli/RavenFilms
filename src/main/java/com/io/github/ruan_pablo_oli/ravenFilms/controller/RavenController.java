990b894a347f768824f361b1922be8d92682fea0
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-31 17:52:41 -0300

    Using bootstrap for styling page html

--- src/main/java/com/io/github/ruan_pablo_oli/ravenFilms/controller/RavenController.java
+++ src/main/java/com/io/github/ruan_pablo_oli/ravenFilms/controller/RavenController.java
@@ -4,6 +4,7 @@ package com.io.github.ruan_pablo_oli.ravenFilms.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.io.github.ruan_pablo_oli.ravenFilms.controller.DTO.FilmDTO;
 import com.io.github.ruan_pablo_oli.ravenFilms.service.RavenService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +23,19 @@ public class RavenController {
     }
 
 
-    @GetMapping("/films")
+    @GetMapping("")
     public String buscarFilmes(Model model) throws JsonProcessingException {
 
         List<FilmDTO> films = ravenService.consumirTMDB();
         model.addAttribute("films",films);
         return "films";
+
+    }
+
+    @GetMapping("/films")
+    public ResponseEntity<?> buscarFilmes() {
+        List<FilmDTO> films = ravenService.consumirTMDB();
+        return ResponseEntity.ok().body(films);
 
     }
 
